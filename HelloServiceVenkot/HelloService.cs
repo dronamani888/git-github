e9a2edeba2cb01039cc26809968fdd31638f7d9b
dronamani888 <drona.com@gmail.com>
2017-11-05 15:10:37 -0600

    Wcf service

---
+++ HelloServiceVenkot/HelloService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace HelloServiceVenkot
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "HelloService" in both code and config file together.
+    public class HelloService : IHelloService
+    {
+        public string GetMessage(string name)
+        {
+           return "Hello" + name;
+        }
+    }
+}
